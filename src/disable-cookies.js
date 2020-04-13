d03453c920708ce7e4a74d57276e946bc0ca148b
Yath Seanghay <seanghay.dev@gmail.com>
2020-04-13 17:56:31 +0700

    feat(log): make it fancy

--- src/disable-cookies.js
+++ src/disable-cookies.js
@@ -1,9 +1,9 @@
 module.exports = function(context) {
     const isCookiesDisabled = context.request.getEnvironmentVariable('DISABLE_COOKIES');
-    if (!isCookiesDisabled) {
+    context.request.settingSendCookies(!!!isCookiesDisabled);
+    if (isCookiesDisabled) {
+        console.log(`[cookies] Disable sending cookies.`)
+    } else {
         console.log(`[cookies] Enable sending cookies.`)
-        return;
     }
-    context.request.settingSendCookies(false);
-    console.log(`[cookies] Disable sending cookies.`)
 }
