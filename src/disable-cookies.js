d4e86f1f717369bfc512201b82a1e12f93b0d23f
Yath Seanghay <seanghay.dev@gmail.com>
2020-04-13 17:19:10 +0700

    feat(log): Log when enable sending cookies

--- src/disable-cookies.js
+++ src/disable-cookies.js
@@ -1,6 +1,7 @@
 module.exports = function(context) {
     const isCookiesDisabled = context.request.getEnvironmentVariable('DISABLE_COOKIES');
     if (!isCookiesDisabled) {
+        console.log(`[cookies] Enable sending cookies.`)
         return;
     }
     context.request.settingSendCookies(false);
