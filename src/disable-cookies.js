426425dcf151ab6ff2b2b003e553452cda818604
Seanghay <yathseanghay404@gmail.com>
2020-07-16 11:20:43 +0700

    Update disable-cookies.js

--- src/disable-cookies.js
+++ src/disable-cookies.js
@@ -1,6 +1,6 @@
 module.exports = function(context) {
     const isCookiesDisabled = context.request.getEnvironmentVariable('DISABLE_COOKIES');
-    context.request.settingSendCookies(!!!isCookiesDisabled);
+    context.request.settingSendCookies(!isCookiesDisabled);
     if (isCookiesDisabled) {
         console.log(`[cookies] Disable sending cookies.`)
     } else {
